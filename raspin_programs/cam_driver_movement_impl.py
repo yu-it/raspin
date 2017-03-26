6ae0ecb203a43b9d883f284ec9a219272a3d3986
fwje7971 <fwje7971@hotmail.com>
2017-03-26 22:27:31 +0900

    ss

--- raspin_programs/cam_driver_movement_impl.py
+++ raspin_programs/cam_driver_movement_impl.py
@@ -1,25 +1,34 @@
 import raspin.pysical_util as pi
+from multiprocessing import Process, Value
 
+p = None
 current_duty = 0
+direction = 0
+num = Value('d', 0.0)
+def move(initial, direction):
+    while True:
+        initial.value += direction
+        pi.pwm(initial.value)
+
+
 def up():
     pass
 def down():
     pass
 
 def right():
-    global current_duty
-    current_duty += 10
-    current_duty = min(current_duty , 95)
-    current_duty = max(current_duty , 5)
-    pi.pwm(current_duty)
+    global p, num
+    if p <> None:
+        p.terminate()
+    p = Process(target=move, args=[num, 0.01])
+    p.start()
 def left():
-    global current_duty
-    current_duty -= 10
-    current_duty = min(current_duty , 95)
-    current_duty = max(current_duty , 5)
-    pi.pwm(current_duty )
+    global p, num
+    if p <> None:
+        p.terminate()
+    p = Process(target=move, args=[num, -0.01])
+    p.start()
 
 def stop():
-    current_duty = 5
-    pi.pwm(current_duty )
-
+    if p <> None:
+        p.terminate()
