d6ee99a300f66b97d6d6fbe392a45373f9180cbd
fwje7971 <fwje7971@hotmail.com>
2017-04-16 09:56:24 +0900

    改行コードミスってた

--- raspin_programs/launch.sh
+++ raspin_programs/launch.sh
@@ -1,7 +1,7 @@
-mkdir #logs
-nohup sudo python physical_driver.py >> #logs/physical_driver.log 2>&1
-nohup sudo python uvccam-ex-service.py >> #logs/cams.log 2>&1
-nohup sudo python cpu-service.py >> #logs/cpus.log 2>&1
-nohup sudo python mem-service.py >> #logs/mems.log 2>&1
-nohup sudo python power-service.py >> #logs/powers.log 2>&1
+mkdir logs
+nohup sudo python physical_driver.py >> logs/physical_driver.log 2>&1
+nohup sudo python uvccam-ex-service.py >> logs/cams.log 2>&1
+nohup sudo python cpu-service.py >> logs/cpus.log 2>&1
+nohup sudo python mem-service.py >> logs/mems.log 2>&1
+nohup sudo python power-service.py >> logs/powers.log 2>&1
 
