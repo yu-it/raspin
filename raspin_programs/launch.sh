6a43a3c2af3a65f3ba6ba0b076a71537161c8fc0
fwje7971 <fwje7971@hotmail.com>
2017-12-02 08:56:11 +0900

    ちょっとフォルダ構成変えた

--- raspin_programs/launch.sh
+++
@@ -1,7 +0,0 @@
-mkdir logs
-nohup sudo python physical_driver.py >> logs/physical_driver.log &
-nohup sudo python uvccam-ex-service.py >> logs/cams.log &
-nohup sudo python cpu-service.py >> logs/cpus.log &
-nohup sudo python mem-service.py >> logs/mems.log &
-nohup sudo python power-service.py >> logs/powers.log &
-
