b664dd2178d973af8c0aaacf6c8b2930c8a64904
yu-it <yousuke0303@gmail.com>
2018-01-08 17:08:25 +0900

    c

--- public/javascripts/frame_of_process.js
+++ public/javascripts/frame_of_process.js
@@ -5,16 +5,18 @@ function setup_process(process_name,disp_name) {
         if (ifs == undefined || ifs.length == 0) {
             return;
         }
+        var idx = 0;
         ifs.forEach(function(iface) {
             console.log("url:" + resource_path(process_name) + "/ifs" + ", iface:" + iface)
-            append_if(process_name,iface)
+            append_if(process_name,iface, idx % 3 == 0)
+            idx ++;
         })
     })
 }
 function delete_if(iface) {
     $(id2JQId(iface)).remove()
 }
-function append_if(process_name,iface) {
+function append_if(process_name,iface, br) {
 $("#" + escapeSelectorString(process_name) + "_container").append('<div class="' + iface + '" style="background-color:#CCCCC;"/>')
 $.get(resource_path(iface), function(if_data) {
     console.log(if_data)
